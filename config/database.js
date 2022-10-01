052bac640c2a6b7772401f50f7b66739bae65927
ahmed.elgendy <ahmed.elgendy@MacBook-Pro.local>
2022-10-01 22:54:48 +0200

    edit category schema:

--- config/database.js
+++ config/database.js
@@ -1,14 +1,14 @@
-const mongoose = require('mongoose'); 
-const dbConnection = () =>{
-    mongoose
-  .connect(process.env.DB_URI)
-  .then((conn) => {
-    console.log("database connected " + conn.connection.host);
-  })
-  .catch((err) => {
-    console.log("error connection " + err);
-    process.exit(1);
-  });
-}
+const mongoose = require("mongoose");
+const dbConnection = () => {
+  mongoose
+    .connect(process.env.DB_URI)
+    .then((conn) => {
+      console.log("database connected " + conn.connection.host);
+    })
+    .catch((err) => {
+      console.log("error connection " + err);
+      process.exit(1);
+    });
+};
 
 module.exports = dbConnection;
